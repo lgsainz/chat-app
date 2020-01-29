8f6a7bd574a2ef8fa483a0b209f49f4c5366b526
Leti <lgsainz15@gmail.com>
2020-01-29 15:02:51 -0500

    initial chat client class to connect to chat server

---
+++ src/client/ChatClient.java
@@ -0,0 +1,54 @@
+package client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * This class handles the Client side of the Chat application.
+ */
+public class ChatClient {
+
+    private String hostname;
+    private int port;
+    private String userName;
+
+    /**
+     * ChatClient constructor.
+     * @param hostname
+     * @param port
+     */
+    public ChatClient(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    /**
+     * Connect client to server.
+     */
+    public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+            System.out.println("Now connected to the chat server!");
+        }
+        catch (UnknownHostException e) {
+            System.out.println("Error: server not found " + e.getMessage());
+        }
+        catch (IOException e) {
+            System.out.println("I/O error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Please use the following command: 'java ChatClient <hostname> <port>'");
+        }
+
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        ChatClient client = new ChatClient(hostname, port);
+        client.execute();
+    }
+
+}
