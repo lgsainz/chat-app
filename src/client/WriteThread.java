159a59788e619c52d2dff11147b8e25ebeac1a38
Leti <lgsainz15@gmail.com>
2020-01-29 15:47:44 -0500

    created thread class for client to write to server

--- src/client/WriteThread.java
+++ src/client/WriteThread.java
@@ -1,4 +1,27 @@
 package client;
 
-public class WriteThread {
+import java.net.Socket;
+
+/**
+ * This thread reads the user's input and sends it to the server. It will run in an infinite
+ * loop until the user exits the chat room by typing "peace".
+ */
+public class WriteThread extends Thread {
+
+    private Socket socket;
+    private ChatClient client;
+
+    public WriteThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+    }
+
+    /**
+     * This block of code will execute when we start() a thread.
+     */
+    @Override
+    public void run() {
+
+    }
+
 }
