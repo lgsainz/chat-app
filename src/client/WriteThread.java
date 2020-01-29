23080f26748bd91583520cf926bf5c1f8ac51571
Leti <lgsainz15@gmail.com>
2020-01-29 16:20:56 -0500

    added username validation for new connected clients

--- src/client/WriteThread.java
+++ src/client/WriteThread.java
@@ -1,6 +1,11 @@
 package client;
 
+import java.io.Console;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * This thread reads the user's input and sends it to the server. It will run in an infinite
@@ -10,10 +15,14 @@ public class WriteThread extends Thread {
 
     private Socket socket;
     private ChatClient client;
+    private PrintWriter writer;
+    private Set<String> userNames = new HashSet<>();
 
     public WriteThread(Socket socket, ChatClient client) {
         this.socket = socket;
         this.client = client;
+
+        
     }
 
     /**
@@ -21,7 +30,30 @@ public class WriteThread extends Thread {
      */
     @Override
     public void run() {
-        
-    }
 
+        Console console = System.console();
+        String userName = console.readLine("Enter your username: ");
+
+        if (!userNames.contains(userName)) {
+            client.setUserName(userName);
+            writer.println(userName);
+
+            String text;
+            do {
+                text = console.readLine("[" + userName + "]: ");
+                writer.println(text);
+            } while (!text.equals("peace"));
+
+            try {
+                socket.close();
+            }
+            catch (IOException e) {
+                System.out.println("Error writing to the server: " + e.getMessage());
+            }
+
+        }
+        else {
+            System.out.println("That username is taken, please enter a new username.");
+        }
+    }
 }
